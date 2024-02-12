b3c217a079565b58bb506621656ad1f654f84d89
alx-yng <alexyoung0119@gmail.com>
2024-02-11 23:04:41 -0600

    added animations

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -1,9 +1,17 @@
 const lazyLoading = () => {
   const lazyImgs = document.querySelectorAll(".lazy");
+  const sections = document.querySelectorAll("section");
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
+        // section finding
+        let section = entry.target;
+        section.classList.toggle("animate");
+        section.classList.toggle("fadeIn");
+        section.classList.toggle("animate-slow");
+        observer.unobserve(section);
+        // img finding
         let img = entry.target;
         img.src = img.dataset.src;
         img.classList.remove("loading");
@@ -15,6 +23,10 @@ const lazyLoading = () => {
 
   lazyImgs.forEach((img) => {
     observer.observe(img);
+  });
+
+  sections.forEach((section) => {
+    observer.observe(section);
   });
 };
 
